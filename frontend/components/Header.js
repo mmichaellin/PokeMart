67e84f4a42f96b92ee150342b3e8f979c84b159c
Michael <mmichaellin@gmail.com>
2019-03-06 20:29:56 -0500

    next.js, basic homepages, favicon

---
+++ frontend/components/Header.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar'
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="#">PokeMart</a>
+      <Navbar />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
+
+export default Header
---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Home = props => (
+  <div>
+    <p>Index js page</p>
+
+  </div>
+);
+
+export default Home;
