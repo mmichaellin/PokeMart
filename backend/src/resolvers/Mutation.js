1c02688e9f9a438acc8b98510fbb2e65dcc17456
Michael <mmichaellin@gmail.com>
2019-03-18 20:55:34 -0400

    updated data model, schema, and created first mutation/query resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info){
+    // later need to check if user is logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {...args}
+    }, info)
+    return item
+  }
+};
 
 module.exports = mutations;
