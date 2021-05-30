e01cb9b6c1a8e9758c8bc9b6e4d189b3215fcaab
Michael <mmichaellin@gmail.com>
2021-05-30 15:56:02 -0400

    .gitignore fix

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c87fc6299b3b68e0b825";
+/******/ 	var hotCurrentHash = "3e0c46a58ee7b36f0a12";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
